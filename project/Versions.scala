054e19f3c26f3c76a33e1e7406173e97a5da355a
satorg <satorg@gmail.com>
2021-05-16 22:45:42 -0700

    upgrade project dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,6 +2,6 @@ object Versions {
   final val `Scala_2.12` = "2.12.13"
   final val `Scala_2.13` = "2.13.5"
 
-  final val fs2 = "2.4.6"
-  final val specs2 = "4.10.5"
+  final val fs2 = "2.5.6"
+  final val specs2 = "4.11.0"
 }
