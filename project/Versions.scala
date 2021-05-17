0ebde68ef7a1cba50602dd9e260ba2c788a76fb2
satorg <satorg@gmail.com>
2021-05-16 22:40:42 -0700

    bump up Scala versions to 2.12.13 and 2.13.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
-  final val `Scala_2.12` = "2.12.12"
-  final val `Scala_2.13` = "2.13.4"
+  final val `Scala_2.12` = "2.12.13"
+  final val `Scala_2.13` = "2.13.5"
 
   final val fs2 = "2.4.6"
   final val specs2 = "4.10.5"
