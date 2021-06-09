8996019b9d56eb043242fc6132371aca838b303d
satorg <satorg@gmail.com>
2021-06-09 11:50:33 -0700

    Scala 2.13.6 and 2.12.14

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
-  final val `Scala_2.12` = "2.12.13"
-  final val `Scala_2.13` = "2.13.5"
+  final val `Scala_2.12` = "2.12.14"
+  final val `Scala_2.13` = "2.13.6"
 
   final val fs2 = "2.5.6"
   final val specs2 = "4.11.0"
