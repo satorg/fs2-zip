a2cd7d68bb17f325c39332c4592752c6b26461b6
satorg <satorg@gmail.com>
2021-06-09 19:14:33 -0700

    improve some tests

--- src/test/scala/satorg/fs2/zip/ZipUnzipTest.scala
+++ src/test/scala/satorg/fs2/zip/ZipUnzipTest.scala
@@ -21,16 +21,15 @@
 
 package satorg.fs2.zip
 
-import java.io.InputStream
-
 import cats.effect._
-import cats.implicits._
+import cats.syntax.all._
 import fs2._
 import org.scalacheck._
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 import scodec.bits.ByteVector
 
+import java.io.InputStream
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
@@ -38,12 +37,23 @@ class ZipUnzipTest extends Specification with ScalaCheck {
 
   import ZipUnzipTest._
 
-  val testBlocker: Blocker =
+  private val timeout: FiniteDuration = 10.seconds
+  private val testBlocker: Blocker =
     Blocker.liftExecutionContext(ExecutionContext.global)
-  implicit val testCS: ContextShift[IO] =
+  private implicit val testCS: ContextShift[IO] =
     IO.contextShift(ExecutionContext.global)
 
   "unzipPipe" should {
+    val expectedResults =
+      Seq(
+        "dir1/" -> "d41d8cd98f00b204e9800998ecf8427e",
+        "dir1/dir2/" -> "d41d8cd98f00b204e9800998ecf8427e",
+        "file1.txt" -> "a88bf0ec0f06e457593a7aea64fd50dd",
+        "dir1/file2.txt" -> "7c5ef0612dfc2ddab63b00cc0386aa53",
+        "dir1/dir2/file3.txt" -> "0f5fb227947695ca2bfcb3d45b40cb0c",
+        "dir1/dir2/file4.txt" -> "db8bfc02dd287a86eb18cdfcab96c6ac"
+      )
+
     "unzip files and directories from a test zip archive" in {
       io.readInputStream(
         acquireResourceInputStream("/test.zip", testBlocker),
@@ -53,23 +63,31 @@ class ZipUnzipTest extends Specification with ScalaCheck {
         .flatMap { entry =>
           entry.body
             .through(hash.md5)
-            .map(b => f"$b%02x")
-            .foldMonoid
+            .foldMap(b => f"$b%02x")
             .map(entry.path -> _)
         }
         .compile
         .toList
         .unsafeRunSync() must
-        containTheSameElementsAs(
-          Seq(
-            "dir1/" -> "d41d8cd98f00b204e9800998ecf8427e",
-            "dir1/dir2/" -> "d41d8cd98f00b204e9800998ecf8427e",
-            "file1.txt" -> "a88bf0ec0f06e457593a7aea64fd50dd",
-            "dir1/file2.txt" -> "7c5ef0612dfc2ddab63b00cc0386aa53",
-            "dir1/dir2/file3.txt" -> "0f5fb227947695ca2bfcb3d45b40cb0c",
-            "dir1/dir2/file4.txt" -> "db8bfc02dd287a86eb18cdfcab96c6ac"
-          )
-        )
+        containTheSameElementsAs(expectedResults)
+    }
+    "allow processing unzipped entries in parallel" in {
+      io.readInputStream(
+        acquireResourceInputStream("/test.zip", testBlocker),
+        256,
+        testBlocker
+      ).through(unzipPipe(testBlocker))
+        .map { entry =>
+          entry.body
+            .through(hash.md5)
+            .foldMap(b => f"$b%02x")
+            .map(entry.path -> _)
+        }
+        .parJoinUnbounded
+        .compile
+        .toList
+        .unsafeRunSync() must
+        containTheSameElementsAs(expectedResults)
     }
   }
   "zipPipe and unzipPipe" should {
@@ -132,16 +150,17 @@ class ZipUnzipTest extends Specification with ScalaCheck {
           // prepare and run the stream
           .compile
           .toVector
-          .unsafeRunTimed(5.seconds) must beSome(===(sourceEntries))
+          .unsafeRunTimed(timeout) must beSome(===(sourceEntries))
       }
     }
   }
 }
 
 object ZipUnzipTest {
-  private def acquireResourceInputStream(name: String, blocker: Blocker)(
-      implicit cs: ContextShift[IO]
-  ): IO[InputStream] =
+  private def acquireResourceInputStream(
+      name: String,
+      blocker: Blocker
+  )(implicit cs: ContextShift[IO]): IO[InputStream] =
     blocker
       .delay[IO, InputStream] { getClass.getResourceAsStream(name) }
       .ensure(throw new RuntimeException(s"cannot load '$name'"))(_ != null)
