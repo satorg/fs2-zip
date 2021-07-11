bbbbbc1e49a509e9c3e147dbeca5eef33377757c
satorg <satorg@gmail.com>
2021-07-10 17:13:00 -0700

    update: fs2 2.5.9, specs2 4.12.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,8 +2,8 @@ object Versions {
   final val `Scala_2.12` = "2.12.14"
   final val `Scala_2.13` = "2.13.6"
 
-  final val fs2 = "2.5.6"
-  final val specs2 = "4.12.0"
+  final val fs2 = "2.5.9"
+  final val specs2 = "4.12.3"
   final val catsEffectTesting = "0.5.4"
   final val scalacheckEffect = "1.0.2"
 }
