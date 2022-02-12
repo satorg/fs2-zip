781c39b5aa2ea6e489933817d38a2c29dfefa33b
satorg <satorg@gmail.com>
2022-02-12 07:46:18 -0800

    update plugins and dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
 
-  final val Scala2_12 = "2.12.14"
-  final val Scala2_13 = "2.13.6"
+  final val Scala2_12 = "2.12.15"
+  final val Scala2_13 = "2.13.8"
 
-  final val Fs2 = "2.5.9"
-  final val MUnit = "0.7.28"
-  final val MUnitCatsEffect = "1.0.5"
-  final val ScalacheckEffect = "1.0.2"
+  final val Fs2 = "2.5.10"
+  final val MUnit = "0.7.29"
+  final val MUnitCatsEffect = "1.0.7"
+  final val ScalacheckEffect = "1.0.3"
 }
